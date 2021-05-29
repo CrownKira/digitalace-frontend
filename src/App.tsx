6f11957155d4a61c34743d5d4192aef4c83994d1
CrownKira <t.karwi@yahoo.com>
2021-05-29 21:37:42 +0800

    Add translation feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { useEffect } from 'react';
 import { Admin, DataProvider } from 'react-admin';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
 
 import authProvider from './authProvider';
 import { Login } from './layout';
+import englishMessages from './i18n/en';
+
+const i18nProvider = polyglotI18nProvider((locale) => {
+  // if (locale === 'fr') {
+  //   return import('./i18n/fr').then(messages=>messages.default);
+  // }
+
+  return englishMessages;
+}, 'en');
 
 interface AppProps {
   onUnmount: () => void;
@@ -22,7 +32,7 @@ const App = ({ onUnmount, dataProvider }: AppProps) => {
       // dashboard={}
       loginPage={Login}
       // layout={}
-      // i18nProvider={}
+      i18nProvider={i18nProvider}
     ></Admin>
   );
 };
