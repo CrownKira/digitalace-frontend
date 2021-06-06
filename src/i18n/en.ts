5dd37fedec09ead6b0acdc6c4c8b3eecf970f559
CrownKira <t.karwi@yahoo.com>
2021-06-06 14:42:36 +0800

    Change welcome title

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -27,7 +27,7 @@ const customEnglishMessages: TranslationMessages = {
           'by %{customer_name}, one item |||| by %{customer_name}, %{nb_items} items',
       },
       welcome: {
-        title: 'Welcome to DigitaLAce demo',
+        title: 'Site under construction',
         subtitle:
           "Feel free to explore and modify the data - it's local to your computer, and will reset each time you reload.",
         ra_button: 'GitHub repo',
