a04300fad9ad243a9893067c43f411dd78ca8a42
CrownKira <t.karwi@yahoo.com>
2021-06-08 01:17:33 +0800

    Fix api url

--- src/dataProvider/backend.ts
+++ src/dataProvider/backend.ts
@@ -2,6 +2,6 @@ import simpleRestProvider from 'ra-data-simple-rest';
 import { baseURL } from '../apis/backend';
 
 // TODO: add env var for host
-const restProvider = simpleRestProvider(`${baseURL}/api/`);
+const restProvider = simpleRestProvider(`${baseURL}/api`);
 
 export default restProvider;
