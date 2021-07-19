95df06da68cd3790bc637a6f66d8996b13eefeac
CrownKira <t.karwi@yahoo.com>
2021-07-19 18:30:30 +0800

    Add migration announcement

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import { useMediaQuery, Theme, Link } from "@material-ui/core";
 import { Alert, AlertTitle } from "@material-ui/lab";
 
 import { Welcome } from "./Welcome";
+import { Separator } from "../utils/components/Divider";
 
 const styles = {
   flex: { display: "flex" },
@@ -17,42 +18,68 @@ const email = "t.karwi@yahoo.com";
 
 const ProgressAlert: FC = () => {
   return (
-    <Alert severity="warning">
-      <AlertTitle>Work in Progress</AlertTitle>
-      Here is a non-exhaustive and nonchronological list of features we are
-      working on:
-      <ul>
-        <li>Inventory Reports</li>
-        <li>PDF/Print</li>
-        <li>Import Data</li>
-        <li>Email Verification on Account Creation</li>
-        <li>Delivery Order System</li>
-        <li>Data Visualization</li>
-        <li>Receives</li>
-        <li>Purchase Orders</li>
-        <li>Inventory adjustments</li>
-        <li>Payroll management</li>
-        <li>Attendance management</li>
-        <li>Show view for all tabs</li>
-      </ul>
-      Want to suggest a feature? Feel free to drop us an email at{" "}
-      <Link
-        className="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary"
-        href={`mailto:${email}`}
-        onClick={stopPropagation}
-      >
-        {email}
-      </Link>
-      <br />
-      Please feel free to file any issues that you have encountered{" "}
-      <Link
-        className="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary"
-        href="https://github.com/CrownKira/digitalace/issues"
-        target="_blank"
-      >
-        here
-      </Link>
-    </Alert>
+    <>
+      <Alert severity="info">
+        <AlertTitle>We are moving</AlertTitle>
+        We are migrating to{" "}
+        <Link
+          className="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary"
+          href="http://eizea.com/"
+          target="_blank"
+        >
+          EIZEA.COM
+        </Link>
+        ! You can pronounce it as /ˈiːzi/ or /ˈeɪ.zɪə/... up to you! Let us know
+        what you think about our new name! Meanwhile, we are conducting our
+        final round of user testing. We would appreciate it if you could set
+        aside your precious 15min to complete our{" "}
+        <Link
+          className="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSdH_svSkLrqfGPoOAGCQ1-SRT4Vw6mBpkGTqGtiXmtkHELUng/viewform"
+          target="_blank"
+        >
+          survey
+        </Link>
+        . We embrace any critiques with open arms!
+      </Alert>
+      <Separator />
+      <Alert severity="warning">
+        <AlertTitle>Work in Progress</AlertTitle>
+        Here is a non-exhaustive and nonchronological list of features we are
+        working on:
+        <ul>
+          <li>Inventory Reports</li>
+          <li>PDF/Print</li>
+          <li>Import Data</li>
+          <li>Email Verification on Account Creation</li>
+          <li>Delivery Order System</li>
+          <li>Data Visualization</li>
+          <li>Receives</li>
+          <li>Purchase Orders</li>
+          <li>Inventory adjustments</li>
+          <li>Payroll management</li>
+          <li>Attendance management</li>
+          <li>Show view for all tabs</li>
+        </ul>
+        Want to suggest a feature? Feel free to drop us an email at{" "}
+        <Link
+          className="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary"
+          href={`mailto:${email}`}
+          onClick={stopPropagation}
+        >
+          {email}
+        </Link>
+        <br />
+        Please feel free to file any issues that you have encountered{" "}
+        <Link
+          className="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary"
+          href="https://github.com/CrownKira/digitalace/issues"
+          target="_blank"
+        >
+          here
+        </Link>
+      </Alert>
+    </>
   );
 };
 
