ff33ac7e7c768b18e106df3f72253e7794fd4050
CrownKira <t.karwi@yahoo.com>
2021-07-25 19:26:52 +0800

    Fix dashboard layout

--- src/dashboard/ MonthlyRevenue.tsx
+++ src/dashboard/ MonthlyRevenue.tsx
@@ -1,18 +1,27 @@
 import React, { FC } from "react";
 import DollarIcon from "@material-ui/icons/AttachMoneyTwoTone";
 import { useTranslate } from "react-admin";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { CardWithIcon } from "./CardWithIcon";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: "300px",
+  },
+}));
 
 interface Props {
   value?: string;
 }
 
 export const MonthlyRevenue: FC<Props> = ({ value }) => {
+  const classes = useStyles();
   const translate = useTranslate();
 
   return (
     <CardWithIcon
+      className={classes.root}
       to="/invoices"
       icon={DollarIcon}
       title={translate("pos.dashboard.monthly_revenue")}
--- src/dashboard/NbNewInvoices.tsx
+++ src/dashboard/NbNewInvoices.tsx
@@ -3,20 +3,29 @@ import ShoppingCartIcon from "@material-ui/icons/ShoppingCartTwoTone";
 import { useTranslate } from "react-admin";
 import { stringify } from "query-string";
 import { subDays } from "date-fns";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { CardWithIcon } from "./CardWithIcon";
 import { dateFormatter } from "../utils";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: "300px",
+  },
+}));
 
 interface Props {
   value?: number;
 }
 
 export const NbNewInvoices: FC<Props> = ({ value }) => {
+  const classes = useStyles();
   const translate = useTranslate();
   const aMonthAgo = subDays(new Date(), 30);
 
   return (
     <CardWithIcon
+      className={classes.root}
       to={{
         pathname: "/invoices",
         search: stringify({
